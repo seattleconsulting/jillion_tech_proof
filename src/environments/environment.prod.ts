77ae02e5eee5384aebdd484efc4279b5f4f010f0
Chaw Su Su Nyein <chawsusunyein@ChawnoMacBook-Pro.local>
2020-11-04 11:39:38 +0900

    install firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: 'AIzaSyAO3Kq8uXMbnISDdRgkb3v5JFuGjIMq9H8',
+    authDomain: 'awsome-chat-b75b2.firebaseapp.com',
+    databaseURL: 'https://awsome-chat-b75b2.firebaseio.com',
+    projectId: 'awsome-chat-b75b2',
+    storageBucket: 'awsome-chat-b75b2.appspot.com',
+    messagingSenderId: '26873143171'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    apiKey: 'AIzaSyAO3Kq8uXMbnISDdRgkb3v5JFuGjIMq9H8',
+    authDomain: 'awsome-chat-b75b2.firebaseapp.com',
+    databaseURL: 'https://awsome-chat-b75b2.firebaseio.com',
+    projectId: 'awsome-chat-b75b2',
+    storageBucket: 'awsome-chat-b75b2.appspot.com',
+    messagingSenderId: '26873143171'
+  }
 };
 
 /*
